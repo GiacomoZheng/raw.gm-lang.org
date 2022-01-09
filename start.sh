96253a60250c9716680a39fd3dcb37ecf3647795
GiacomoZheng <giacomo821@outlook.com>
2022-01-09 16:41:03 +0000

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 echo "TODO test it"
 
 cd /home/giacomo/
-git clone https://github.com/GiacomoZheng/raw.gm-lang.org.git
+git pull https://github.com/GiacomoZheng/raw.gm-lang.org.git
 
 cd /home/giacomo/raw.gm-lang.org
-git https://github.com/GiacomoZheng/pygments_gm.git
+git clone https://github.com/GiacomoZheng/pygments_gm.git
 
-cd raw.gm-lang.org/
+cd /home/giacomo/raw.gm-lang.org/
 mkdir src_cache
 mkdir raw
 cd /home/giacomo/raw.gm-lang.org/raw
-git https://github.com/GiacomoZheng/gm.git
+git clone https://github.com/GiacomoZheng/gm.git
 
 cp /home/giacomo/raw.gm-lang.org/raw.service /etc/systemd/system/raw.service
 
